85b1af0810e95ff611e1ae7dfcd15ab8aea94087
neharana4486 <57110340+neharana4486@users.noreply.github.com>
2022-11-29 23:36:21 +0100

    Update account.py

--- account.py
+++ account.py
@@ -1,8 +1,8 @@
 class Account:
     def __init__(self, account_type, account_number, balance): # Account class has attributes account_type, account_number, balance. The 'transactions' is empty list,
         self.account_type= account_type                        # which is populated by calling 'addTransactions' function.
-        self.account_number= account_number                    #
-        self.balance= balance                                  #
+        self.account_number= account_number                    
+        self.balance= balance                                  
         self.transactions = []
 
     def addTransactions(self, transactioin):
